50f2cd5e56bae870349ede753628f1c6a2c84562
Angel Mera Delgado <angelmeradelgado@gmail.com>
2023-12-27 23:27:53 -0500

    actualizacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import  Aside from "./components/Aside";
+import  Pokemons from "./components/Pokemons";
 
 
 function App() {
   return (
-    <>
-      <h1 className="text-2xl font-bold">Pokedex</h1>
-    </>
-  )
+    <section className="bg-[#F6F8FC] h-screen font-outfit overflow-y-auto">
+      <main className="max-m-[1400px] mx-auto grid grid-cols-1 lg:grid-cols-[1fr_350px]">
+        <Pokemons />
+        <Aside />
+      </main>
+    </section>
+  );
 }
 
-export default App
+export default App;
